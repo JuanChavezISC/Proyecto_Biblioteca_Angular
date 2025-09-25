4ffe593af48063220eb323101b802f4d921e9163
Juan Carlos Chavez <juanchavezsn@gmail.com>
2025-09-25 13:35:55 -0600

    Agregar ruta de Libros al routing Module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { AutorComponent } from './components/autor/autor.component';
 import { AutorFormComponent } from './components/autor/autor-form/autor-form.component';
 import { CategoriaComponent } from './components/categoria/categoria.component';
 import { CategoriaFormComponent } from './components/categoria/categoria-form/categoria-form.component';
+import { LibroComponent } from './components/libro/libro.component';
 
 const routes: Routes = [
   {path: '', redirectTo: '/autores', pathMatch: 'full'},
@@ -15,6 +16,8 @@ const routes: Routes = [
   {path: 'categorias', component: CategoriaComponent},
   {path: 'categorias/form', component: CategoriaFormComponent},
   {path: 'categorias/form/:id', component: CategoriaFormComponent},
+  /*Libro Path */
+  {path: 'libros', component:LibroComponent},
 ];
 
 @NgModule({
