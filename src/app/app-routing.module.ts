9a74adec847a9631d95a230034c19985caab6ef9
Juan Carlos Chavez <juanchavezsn@gmail.com>
2025-10-13 10:23:36 -0600

    conexion del componente Usuario

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,8 @@ import { CategoriaComponent } from './components/categoria/categoria.component';
 import { CategoriaFormComponent } from './components/categoria/categoria-form/categoria-form.component';
 import { LibroComponent } from './components/libro/libro.component';
 import { LibroFormComponent } from './components/libro/libro-form/libro-form.component';
+import { UsuarioComponent } from './components/usuario/usuario.component';
+import { UsuarioFormComponent } from './components/usuario/usuario-form/usuario-form.component';
 
 const routes: Routes = [
   {path: '', redirectTo: '/autores', pathMatch: 'full'},
@@ -21,6 +23,11 @@ const routes: Routes = [
   {path: 'libros', component:LibroComponent},
   {path: 'libros/form', component:LibroFormComponent},
   {path: 'libros/form/:id', component:LibroFormComponent},
+  
+  /*Usuario Path */
+  {path: 'usuarios', component:UsuarioComponent},
+  {path: 'usuarios/form', component:UsuarioFormComponent},
+  {path: 'usuarios/form/:id', component:UsuarioFormComponent},
 ];
 
 @NgModule({
